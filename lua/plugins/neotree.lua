4285a4d37efb5b156b68a24a490cf96650e4788f
Cody Mikol <codymikol@gmail.com>
2024-01-09 10:39:59 -0500

    feat: show hidden files
    this adjusts the neotree config to show hidden files
    
    Fixes N/A

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- This will make it so "hidden" files are shown, but in a different color
+      },
+    },
+  },
+}
