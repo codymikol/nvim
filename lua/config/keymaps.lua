6ee00a158e95eb1879cffe93937ec9cbb2813da9
Cody Mikol <hi@codymikol.com>
2024-10-21 08:41:23 -0400

    feat(keymap): new keymaps
    bo -- buffer only, close other buffers
    and special tab completion mapping for luasnip
    
    Fixes N/A

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,40 +34,69 @@ map("n", "<leader>ts", function()
   vim.cmd("w | Neotest summary")
 end)
 
-vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
   expr = true,
-  replace_keycodes = false
+  replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
 
 -- Disable docs on F1
-vim.api.nvim_set_keymap('n', '<F1>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<F1>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F1>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<F1>", "<Nop>", { noremap = true, silent = true })
 
 -- Refactoring.nvim
 
-vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end)
-vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end)
+vim.keymap.set("x", "<leader>re", function()
+  require("refactoring").refactor("Extract Function")
+end)
+vim.keymap.set("x", "<leader>rf", function()
+  require("refactoring").refactor("Extract Function To File")
+end)
 -- Extract function supports only visual mode
-vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end)
+vim.keymap.set("x", "<leader>rv", function()
+  require("refactoring").refactor("Extract Variable")
+end)
 -- Extract variable supports only visual mode
-vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end)
+vim.keymap.set("n", "<leader>rI", function()
+  require("refactoring").refactor("Inline Function")
+end)
 -- Inline func supports only normal
-vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end)
+vim.keymap.set({ "n", "x" }, "<leader>ri", function()
+  require("refactoring").refactor("Inline Variable")
+end)
 -- Inline var supports both normal and visual mode
 
-vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end)
-vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+vim.keymap.set("n", "<leader>rb", function()
+  require("refactoring").refactor("Extract Block")
+end)
+vim.keymap.set("n", "<leader>rbf", function()
+  require("refactoring").refactor("Extract Block To File")
+end)
 -- Extract block supports only normal mode
 
 -- Buffer Maps
 
-map("n", "<leader>bo", function ()
+map("n", "<leader>bo", function()
   -- write a function that will delete all buffers except the one open
   local current_buf = vim.api.nvim_get_current_buf()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if buf ~= current_buf and vim.api.nvim_buf_get_option(buf, 'buftype') == '' then
+    if buf ~= current_buf and vim.api.nvim_buf_get_option(buf, "buftype") == "" then
       vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
 end, { desc = "Close other buffers" })
+
+-- LuaSnip maps
+
+vim.api.nvim_set_keymap(
+  "i",
+  "<S-CR>",
+  [[<cmd>lua require('luasnip').expand_or_jump()<CR>]],
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "s",
+  "<S-CR>",
+  [[<cmd>lua require('luasnip').expand_or_jump()<CR>]],
+  { noremap = true, silent = true }
+)
