56d6f39ebc1470e00b9b8b1e08ffd6d2f2c781d3
Cody Mikol <cody.mikol@boltstech.com>
2024-10-24 05:34:32 -0400

    chore(cmp): remove notifications
    spamming up my life
    
    Fixes N/A

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,12 +1,8 @@
 return {
   "hrsh7th/nvim-cmp",
   opts = function(_, opts)
-    vim.notify(vim.inspect(opts))
-
     local cmp = require("cmp")
     local luasnip = require("luasnip")
-
-    vim.notify("CR")
 
     opts.mapping["<CR>"] = cmp.mapping(function(fallback)
       if luasnip.locally_jumpable(1) then
@@ -19,21 +15,16 @@ return {
     end)
 
     opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-      vim.notify("TABBIN")
       if luasnip.locally_jumpable(1) then
-        vim.notify("locally jumpable!")
         luasnip.jump(1)
       elseif cmp.visible() then
-        vim.notify("cmp selectable")
         cmp.select_next_item()
       else
-        vim.notify("fallback")
         fallback()
       end
     end, { "i", "s" })
 
     opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
-      vim.notify("S-Tab")
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.locally_jumpable(-1) then
